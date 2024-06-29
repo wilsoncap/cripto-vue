6404de2d30e9dc86ce16625a497569646f632fe9
Wilson Capacho <wiston.98@hotmail.com>
2024-06-28 21:00:38 -0500

    conusmiendo api

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,46 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
+import { ref, onMounted} from "vue"
+
+const monedas = ref([
+      { codigo: 'USD', texto: 'Dolar de Estados Unidos'},
+      { codigo: 'MXN', texto: 'Peso Mexicano'},
+      { codigo: 'EUR', texto: 'Euro'},
+      { codigo: 'GBP', texto: 'Libra Esterlina'},
+])
+
+const cryptomonedas = ref([]);
+
+onMounted(()=>{
+const url = "https://min-api.cryptocompare.com/data/top/totalvolfull?limit=10&tsym=USD";
+fetch(url)
+    .then(respuesta => respuesta.json())
+    .then(({Data}) => cryptomonedas.value = Data)
+})
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+    <div class="contenedor">
+        <h1 class="titulo">Cotizador de <span>Criptomonedas</span></h1>
+        
+        <div class="contenido">
+            <form class="formulario">
+                <div class="campo">
+                <label for="moneda">Moneda:</label>
+                <select id="moneda">
+                    <option value="">-- Selecciona --</option>
+                    <option 
+                    v-for="moneda in monedas" 
+                    :key="moneda.codigo"
+                    :value="moneda.codigo"
+                    >{{ moneda.texto }}</option>
+                </select>
+            </div>
+            </form>
+            
+        </div>
+    </div>
 </template>
 
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
+
 </style>
